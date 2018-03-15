9372e25c7e5b71b974e6f6cf64e14b807c8598c2
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-15 14:49:40 -0500

    Paso 4- Se modifica el codigo para probar el caso String con multiples numeros(Verde)

--- Calculadora.py
+++ Calculadora.py
@@ -2,9 +2,15 @@ __author__= 'Vivian'
 
 class Calculadora:
     def sumar(self,cadena):
+        suma = 0
         if cadena=="":
             return 0
         elif "," in cadena:
-            return int(cadena[0])+int(cadena[2])
+            numeros = cadena.split(",")
+            print suma
+            for num in numeros:
+                suma = suma + int(num)
+                print suma
+            return suma
         else:
             return int(cadena)
--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -5,12 +5,16 @@ from Calculadora import Calculadora
 class CalculadoraTest(TestCase):
     def test_sumar(self):
         self.assertEqual(Calculadora().sumar(""), 0, "Cadena vacia")
+
     def test_sumar_unacadena(self):
-        self.assertEqual(Calculadora().sumar("1"), 1 , "Un numero ")
+        self.assertEqual(Calculadora().sumar("1"), 1 , "Un numero")
+
     def test_sumar_cadenaConUnNumero(self):
         self.assertEqual(Calculadora().sumar("1"), 1, "Un numero")
         self.assertEqual(Calculadora().sumar("2"), 2, "Un numero diferente")
-    def test_sumar_unacadena(self):
-        self.assertEqual(Calculadora().sumar("1,3"), 4, "Dos numeros")
-    def test_sumar_unacadena(self):
-        self.assertEqual(Calculadora().sumar("5,4,2,1"), 12, "Mutliples numeros")
+
+    def test_sumar_cadenaConDosNumero(self):
+        self.assertEqual(Calculadora().sumar("1,3"),4, "Dos numeros")
+
+    def test_sumar_cadenaConMultiplesNumeros(self):
+        self.assertEqual(Calculadora().sumar("5,2,4,1"),12,"Mutliples numeros")
